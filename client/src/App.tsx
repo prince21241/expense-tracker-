61ea9d1be407bf5078cfa37a9df3a6c09abbb7d0
PrinceRaval <divya.raval92@gmail.com>
2025-06-21 14:19:18 -0400

    Add financial record form with user context and form state handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,16 @@
 
 import './App.css'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { DashBoard } from './pages/dashboard';
+import { Auth } from './pages/auth';
 
 function App () {
   return (
     <Router>
       <div className="app-container">
         <Routes>
-            <Route path='/' element={<h1>DashBoard </h1>} />
-            <Route path='/auth' element={<h1>Sign In </h1>} />
+            <Route path='/' element={<DashBoard/>} />
+            <Route path='/auth' element={<Auth/>} />
         </Routes>
       </div>
     </Router>
